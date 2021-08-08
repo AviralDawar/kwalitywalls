f1ae4cd8f7a463b91e09df10a3805acf33d17d2e
AviralDawar <88610201+AviralDawar@users.noreply.github.com>
2021-08-08 18:08:34 +0530

    made types.py

--- Simple-Assembler/types.py
+++ Simple-Assembler/types.py
@@ -1,1 +1,23 @@
-#file for storing the types of all th instructions
+#file for storing the types of all the instructions
+add = 4
+sub = 4
+mov = 3
+ld =  4
+st =  4
+mul = 4
+div = 3
+rs =  3
+ls =  3
+xor = 4
+or =  4
+and = 4
+not = 3
+cmp = 3
+jmp = 2
+jlt = 2
+jgt = 2
+je =  2
+hlt = 1
+
+
+
