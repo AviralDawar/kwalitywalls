ca5744571e8fbc8f3b881b1d3e244d5fecdbcd0e
AviralDawar <88610201+AviralDawar@users.noreply.github.com>
2021-08-27 22:39:32 +0530

    thankyou maam

--- SimpleSimulator/simpleSimulator.py
+++ SimpleSimulator/simpleSimulator.py
@@ -1,8 +1,6 @@
 import sys
 import matplotlib.pyplot as plt
-input_list = list(map(str, sys.stdin.readlines()))
-# f = open('sys.txt', mode='r+')
-# input_list = f.readlines()
+input_list = list(map(str, sys.stdin.readlines())))
 counter=len(input_list)
 list_memory=input_list
 reg={'000':'R0','001':'R1','010':'R2','011':'R3','100':'R4','101':'R5','110':'R6','111':'FLAGS'}
@@ -26,101 +24,96 @@ while(PC<len(input_list)):
     PC_and_regvals=[]
     flag_val=reg_value["FLAGS"]
     reg_value["FLAGS"]=0
+    
+    #add
     if(x[0:5]=="00000"):
-        #add
         reg1=reg[x[7:10]]
         reg2=reg[x[10:13]]
         reg3=reg[x[13:16]]
         reg_value[reg1]=reg_value[reg2]+reg_value[reg3]
-    
-    
         if(reg_value[reg1]>=65536):
             reg_value["FLAGS"]+=8
             reg_value[reg1]= reg_value[reg1]%65536
-   
-    if(x[0:5]=="00001"):
-        #subtract
+    #subtract
+    if(x[0:5]=="00001"):  
         reg1=reg[x[7:10]]
         reg2=reg[x[10:13]]
         reg3=reg[x[13:16]]
-        reg_value[reg1]=reg_value[reg2]-reg_value[reg3]
-        
-        
+        reg_value[reg1]=reg_value[reg2]-reg_value[reg3] 
         if(reg_value[reg1]<0):
             reg_value["FLAGS"]+=8
             reg_value[reg1]=0
-            
+    
+    #multiply  
     if(x[0:5]=="00110"):
-        #multiply
         reg1=reg[x[7:10]]
         reg2=reg[x[10:13]]
         reg3=reg[x[13:16]]
         reg_value[reg1]=reg_value[reg2]*reg_value[reg3]
-        
-        
         if(reg_value[reg1]>=65536 ):
             reg_value["FLAGS"]+=8
             reg_value[reg1]= reg_value[reg1]%65536
 
+    #xor
     if(x[0:5]=="01010"):
-        #xor
         reg1=reg[x[7:10]]
         reg2=reg[x[10:13]]
         reg3=reg[x[13:16]]
         reg_value[reg1]=reg_value[reg2]^reg_value[reg3]
     
+    #or
     if(x[0:5]=="01011"):
-        #or
         reg1=reg[x[7:10]]
         reg2=reg[x[10:13]]
         reg3=reg[x[13:16]]
         reg_value[reg1]=reg_value[reg2]|reg_value[reg3]
     
+    #and
     if(x[0:5]=="01100"):
-        #and
         reg1=reg[x[7:10]]
         reg2=reg[x[10:13]]
         reg3=reg[x[13:16]]
         reg_value[reg1]=reg_value[reg2] & reg_value[reg3]
-
+    
+    #mov_imm
     if(x[0:5]=="00010"):
-        #mov_imm
         reg1=reg[x[5:8]]
         imm=int(x[8:16],2)
         reg_value[reg1]=imm
-
+    
+    #mov_reg
     if(x[0:5]=="00011"):
-        #mov_reg
         reg1=reg[x[10:13]]
         reg2=reg[x[13:16]]
         if(reg2=="FLAGS"):
             reg_value[reg1]=flag_val
         else:
             reg_value[reg1]=reg_value[reg2]
-        
+    
+    #left_shift  
     if(x[0:5]=="01001"):
-        #left_shift
         reg1=reg[x[5:8]]
         imm=int(x[8:16],2)
         reg_value[reg1]= int(reg_value[reg1])<<imm
         if(reg_value[reg1]>=65536):
             reg_value["FLAGS"]+=8
             reg_value[reg1]= reg_value[reg1]%65536
-            
-    if(x[0:5]=="01000"):
-        #right_shift
+    
+    #right_shift      
+    if(x[0:5]=="01000"):  
         reg1=reg[x[5:8]]
         imm=int(x[8:16],2)
         reg_value[reg1]= int(reg_value[reg1])>>imm
-        
-    if(x[0:5]=="00111"):           #divide
+    
+    #divide    
+    if(x[0:5]=="00111"):          
         reg1=reg[x[10:13]]
         reg2=reg[x[13:16]]
         reg_value["R0"]=int(reg_value[reg1]/reg_value[reg2])
         reg_value["R1"]=reg_value[reg1]%reg_value[reg2]
-
-    if(x[0:5]=="01110"):        #compare
-        
+    
+    #compare
+    if(x[0:5]=="01110"):        
         reg1=reg[x[10:13]]
         reg2=reg[x[13:16]]
         if reg_value[reg1]>reg_value[reg2]:
@@ -130,7 +123,8 @@ while(PC<len(input_list)):
         else:
             reg_value["FLAGS"]+=1
     
-    if(x[0:5]=="01101"):           #invert
+    #invert
+    if(x[0:5]=="01101"):          
         reg1=reg[x[10:13]]
         reg2=reg[x[13:16]]
         val=format(reg_value[reg2], '016b')
@@ -142,8 +136,9 @@ while(PC<len(input_list)):
                 i=0
             val_invert+=str(i)
         reg_value[reg1]=int(val_invert,2)
-
-    if(x[0:5] == "00101"):         #stores data from reg to var      #make for, for storing variable value in list_memory
+    
+    #stores data from reg to var
+    if(x[0:5] == "00101"):               
         reg1=reg[x[5:8]]
         val = reg_value[reg1] #val = 5
         if(int(x[8:16],2)-len(input_list) in var_dict):
@@ -154,8 +149,8 @@ while(PC<len(input_list)):
         cycle_ldst.pop()
         cycle_ldst.append(int(x[8:16],2))
        
-    
-    if(x[0:5] == "00100"):         #load data from reg to var
+    #load data from reg to var
+    if(x[0:5] == "00100"):         
         reg1=reg[x[5:8]]
         if(int(x[8:16],2)-len(input_list) in var_dict):
             val=int(var_dict[int(x[8:16],2)-len(input_list)],2)
@@ -167,28 +162,31 @@ while(PC<len(input_list)):
         cycle_ldst.append(int(x[8:16],2))
         reg_value[reg1]=val
 
+    #jmp function
     if(x[0:5] == "01111"):
         #cycle_y[cycle_counter] = PC
         cycle_y.append(PC)
         jump_flag=True
-
+    
+    #jlt function
     if(x[0:5] == "10000"):
         if(flag_val==4):
             cycle_y.append(PC)
             jump_flag=True
             
-
+    #jgt function
     if(x[0:5] == "10001"):
         if(flag_val==2):
             cycle_y.append(PC)
             jump_flag=True
     
-
+    #je function
     if(x[0:5] == "10010"):
         if(flag_val==1):
             cycle_y.append(PC)
             jump_flag=True
 
+    #the below lines will append the output of the program counters and the registers
     PC_bin=format(PC, '08b')
     PC_and_regvals.append(PC_bin)
     PC_and_regvals.append(format(reg_value["R0"], '016b'))
@@ -209,10 +207,11 @@ while(PC<len(input_list)):
 
 for x in output_list:
     print(" ".join(list(map(str,x))))
-    #print( " ".join(list(map(str,x))), file=sys.stderr)
+   
 for i in range(0 , var_count):
     list_memory.append(var_dict[i])
-    
+
+#for acommadating the var_count in the counter
 counter+=var_count
     
 #for completing the output_list
@@ -223,9 +222,12 @@ for i in range(counter,256):
 for x in list_memory:
     x = x.strip('\n')
     print(x)
-    #print(x , file=sys.stderr)
+    
 
+#the below function is for plotting the graph
 plt.scatter(cycle_x , cycle_y, c ="blue")
+
+#the below for loop is for handling the graph for the  ld and st cases
 for i in range(0,len(cycle_ldst)):
     if cycle_ldst[i] != -1:
         plt.plot(i,cycle_ldst[i],'bo')
